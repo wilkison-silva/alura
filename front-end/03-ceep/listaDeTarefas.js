0a4a6e9d14c41de90f6a247f78db4794cd325e43
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2021-12-01 23:47:38 -0400

    feat: Create new item for task list using apprendChild

--- front-end/03-ceep/listaDeTarefas.js
+++ front-end/03-ceep/listaDeTarefas.js
@@ -1,16 +1,20 @@
 const botaoNovoItem = document.querySelector('#botao-novo-item');
 
 const criarNovaTarefa = (evento) => {
+
   evento.preventDefault();
 
   const inputDescricaoTarefa = document.querySelector('#input-descricao-tarefa');
   const textoTarefa = inputDescricaoTarefa.value;
+  const conteudo = `<p class="content">${textoTarefa}</p>`;
+
+  const lista = document.querySelector('#lista-de-tarefas');
+  const novoItemLista = document.createElement('li');
+  novoItemLista.classList.add('task');
+  novoItemLista.innerHTML = conteudo;
+  lista.appendChild(novoItemLista);
+
   inputDescricaoTarefa.value = "";
-
-  const itemLista = document.querySelector('#item-lista');
-  const conteudo = `<p class="content">${textoTarefa}</p>`
-  itemLista.innerHTML = conteudo;
-
 
 }
 
