b32273c1243456cb7dc8ac493283d29c97fda890
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2021-12-02 22:46:11 -0400

    feat: Create component BotaoConcluirTarefa

--- front-end/03-ceep/listaDeTarefas.js
+++ front-end/03-ceep/listaDeTarefas.js
@@ -1,21 +1,34 @@
-const botaoNovoItem = document.querySelector('#botao-novo-item');
 
-const criarNovaTarefa = (evento) => {
+(() => {
 
-  evento.preventDefault();
+  const botaoNovoItem = document.querySelector('#botao-novo-item');
 
-  const inputDescricaoTarefa = document.querySelector('#input-descricao-tarefa');
-  const textoTarefa = inputDescricaoTarefa.value;
-  const conteudo = `<p class="content">${textoTarefa}</p>`;
+  const criarNovaTarefa = (evento) => {
+    evento.preventDefault();
+    const inputDescricaoTarefa = document.querySelector('#input-descricao-tarefa');
+    const textoTarefa = inputDescricaoTarefa.value;
+    const conteudo = `<p class="content">${textoTarefa}</p>`;
 
-  const lista = document.querySelector('#lista-de-tarefas');
-  const novoItemLista = document.createElement('li');
-  novoItemLista.classList.add('task');
-  novoItemLista.innerHTML = conteudo;
-  lista.appendChild(novoItemLista);
+    const lista = document.querySelector('#lista-de-tarefas');
+    const novoItemLista = document.createElement('li');
+    novoItemLista.classList.add('task');
+    novoItemLista.innerHTML = conteudo;
+    novoItemLista.appendChild(BotaoConcluirTarefa());
+    lista.appendChild(novoItemLista);
 
-  inputDescricaoTarefa.value = "";
+    inputDescricaoTarefa.value = "";
+  }
 
-}
+  botaoNovoItem.addEventListener('click',criarNovaTarefa);
 
-botaoNovoItem.addEventListener('click',criarNovaTarefa);
+  const BotaoConcluirTarefa = () => {
+    const botaoConcluirTarefa = document.createElement('button');
+
+    botaoConcluirTarefa.addEventListener('click', (evento) => {
+      const itemDaListaFinalizada = evento.target.parentNode;
+      itemDaListaFinalizada.classList.toggle('done');
+    });
+
+    return botaoConcluirTarefa;
+  }
+})();
