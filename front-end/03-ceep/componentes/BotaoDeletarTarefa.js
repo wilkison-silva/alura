06ad127750373e7ac489b95f41107b4cdcc9d51b
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2021-12-05 22:14:48 -0400

    feat: Add function to delete task

--- front-end/03-ceep/componentes/BotaoDeletarTarefa.js
+++ front-end/03-ceep/componentes/BotaoDeletarTarefa.js
@@ -1,12 +1,16 @@
+import {deletarItem} from '../actions/persistirDados.js';
 import {carregarListaDeTarefas} from '../actions/carregarListaDeTarefas.js';
-const BotaoDeletarTarefa = () => {
+
+const BotaoDeletarTarefa = (index) => {
   const botaoDeletarTarefa = document.createElement('button');
   botaoDeletarTarefa.classList.add('delete-button')
   botaoDeletarTarefa.innerText = 'deletar';
   botaoDeletarTarefa.addEventListener('click', (evento) => {
     const itemDaListaParaExcluir = evento.target.parentNode;
     itemDaListaParaExcluir.remove();
+    deletarItem(index);
     carregarListaDeTarefas();
+
   });
 
   return botaoDeletarTarefa;
