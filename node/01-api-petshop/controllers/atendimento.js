81073a131b9fed84044e47f85cbb230b294faba7
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2021-12-21 20:54:14 -0400

    feat: create method post

--- node/01-api-petshop/controllers/atendimento.js
+++ node/01-api-petshop/controllers/atendimento.js
@@ -1,5 +1,9 @@
 module.exports = (app) => {
   app.get('/atendimentos', (req, res) => {
-    res.send('rodando atendimentos');
+    res.send('rodando atendimentos..........................');
+  });
+
+  app.post('/atendimentos', (req, res) => {
+    res.send(req.body.nome);
   });
 };
