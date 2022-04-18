fc60933af21c4d5095ce1b3bdeed1dcc738cf3be
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2022-04-17 21:45:07 -0400

    feat: adding navController listeners to change title dinamycally

--- android/01-android-navigation-projeto-inicial/app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ android/01-android-navigation-projeto-inicial/app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -1,14 +1,30 @@
 package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController by lazy {
+        findNavController(R.id.produtos_activity_nav_host)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        navController.addOnDestinationChangedListener { navController,
+                                                        navDestination,
+                                                        bundle ->
+            title = navDestination.label
+            when(navDestination.id){
+                R.id.listaProdutos -> supportActionBar?.show()
+                R.id.loginFragment -> supportActionBar?.hide()
+            }
+
+        }
     }
 
 }
