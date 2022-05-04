13d3fb1884108b80264efd055238de92d248c2d8
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2022-05-04 09:23:54 -0400

    first commit

---
+++ android/01 - android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,63 @@
+package br.com.alura.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+
+class ListaProdutosActivity : AppCompatActivity() {
+
+    private val adapter = ListaProdutosAdapter(context = this)
+    private val binding by lazy {
+        ActivityListaProdutosActivityBinding.inflate(layoutInflater)
+    }
+    private val dao by lazy {
+        val db = AppDatabase.instancia(this)
+        db.produtoDao()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraRecyclerView()
+        configuraFab()
+        lifecycleScope.launch {
+            dao.buscaTodos().collect { produtos ->
+                adapter.atualiza(produtos)
+            }
+        }
+    }
+
+    private fun configuraFab() {
+        val fab = binding.activityListaProdutosFab
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = binding.activityListaProdutosRecyclerView
+        recyclerView.adapter = adapter
+        adapter.quandoClicaNoItem = {
+            val intent = Intent(
+                this,
+                DetalhesProdutoActivity::class.java
+            ).apply {
+                putExtra(CHAVE_PRODUTO_ID, it.id)
+            }
+            startActivity(intent)
+        }
+    }
+
+}
