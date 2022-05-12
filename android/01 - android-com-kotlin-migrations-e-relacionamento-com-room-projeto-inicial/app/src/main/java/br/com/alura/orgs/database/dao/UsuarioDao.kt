9848fa3ceabce38a3aba73273a342540bf8fd13e
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2022-05-12 19:39:27 -0400

    feat: sending user logged in to ListaProductActivity

--- android/01 - android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
+++ android/01 - android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
@@ -2,12 +2,20 @@ package br.com.alura.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import br.com.alura.orgs.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsuarioDao {
 
     @Insert
     suspend fun salva(usuario: Usuario)
+
+    @Query("SELECT * FROM Usuario WHERE nome = :usuarioNome AND senha = :usuarioSenha")
+    suspend fun autentica(usuarioNome: String, usuarioSenha: String): Usuario?
+
+    @Query("SELECT * FROM Usuario WHERE id = :usuarioId")
+    fun buscaPorId(usuarioId: String) : Flow<Usuario>
 
 }
--- android/01 - android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/ui/activity/LoginActivity.kt
+++ android/01 - android-com-kotlin-migrations-e-relacionamento-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/ui/activity/LoginActivity.kt
@@ -2,14 +2,22 @@ package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityLoginBinding
 import br.com.alura.orgs.extensions.vaiPara
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    private val usuarioDao by lazy {
+        AppDatabase.instancia(this).usuarioDao()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +32,22 @@ class LoginActivity : AppCompatActivity() {
             val usuario = binding.activityLoginUsuario.text.toString()
             val senha = binding.activityLoginSenha.text.toString()
             Log.i("LoginActivity", "onCreate: $usuario - $senha")
-            vaiPara(ListaProdutosActivity::class.java)
+
+            lifecycleScope.launch {
+                usuarioDao.autentica(
+                    usuarioNome = usuario,
+                    usuarioSenha = senha
+                )?.let { usuario ->
+                    vaiPara(ListaProdutosActivity::class.java){
+                        putExtra("CHAVE_USUARIO_ID", usuario.id)
+                    }
+                }
+                    ?: Toast.makeText(
+                        this@LoginActivity,
+                        "Usuário não encontrado",
+                        Toast.LENGTH_LONG
+                    ).show()
+            }
         }
     }
 
