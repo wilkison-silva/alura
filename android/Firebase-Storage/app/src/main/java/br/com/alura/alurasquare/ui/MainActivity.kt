79a8524c7f6f3437664d78ef5db8711c7919b311
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2022-08-09 20:33:04 -0400

    feat: criando função de enviar imagem para o firebase storage

---
+++ android/Firebase-Storage/app/src/main/java/br/com/alura/alurasquare/ui/MainActivity.kt
@@ -0,0 +1,66 @@
+package br.com.alura.alurasquare.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import br.com.alura.alurasquare.R
+import br.com.alura.alurasquare.databinding.ActivityMainBinding
+import br.com.alura.alurasquare.ui.viewmodel.EstadoAppViewModel
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ListResult
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private val controlador by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
+    }
+    private val viewModel: EstadoAppViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.activityMainToolbar)
+
+
+        val firebaseStorage: FirebaseStorage = Firebase.storage
+
+        lifecycleScope.launchWhenCreated {
+            Log.i("Testando", "passou aqui 1")
+            val listResult: ListResult = firebaseStorage.reference.listAll().await()
+            listResult.items.forEach {
+                Log.i("Testando", "item name -> ${it.name} ")
+                Log.i("Testando", "item url -> ${it.downloadUrl.await()} ")
+            }
+        }
+
+        controlador.addOnDestinationChangedListener { _: NavController, navDestination: NavDestination, _: Bundle? ->
+            title = navDestination.label
+        }
+        viewModel.componentes.observe(this) {
+            it?.let { components ->
+                binding.activityMainToolbar.visibility =
+                    if (components.appBar) VISIBLE
+                    else GONE
+            }
+        }
+    }
+
+}
---
+++ android/Firebase-Storage/app/src/main/java/br/com/alura/alurasquare/ui/viewmodel/FormularioPostViewModel.kt
@@ -0,0 +1,48 @@
+package br.com.alura.alurasquare.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import br.com.alura.alurasquare.model.Post
+import br.com.alura.alurasquare.repository.PostRepository
+import br.com.alura.alurasquare.repository.Resultado
+
+class FormularioPostViewModel(
+    private val repository: PostRepository
+) : ViewModel() {
+
+    fun buscaPost(id: String) = repository.buscaPorId(id).asLiveData()
+
+    fun salva(post: Post, imagemByteArray: ByteArray) =
+        liveData<Resultado<Unit>>(viewModelScope.coroutineContext) {
+            try {
+                val id = repository.salva(post)
+                repository.enviaImagem(id, imagemByteArray)
+                emit(Resultado.Sucesso())
+            } catch (e: Exception) {
+                emit(Resultado.Erro(e))
+            }
+        }
+
+    fun remove(postId: String) =
+        liveData<Resultado<Unit>>(viewModelScope.coroutineContext) {
+            try {
+                repository.remove(postId)
+                emit(Resultado.Sucesso())
+            } catch (e: Exception) {
+                emit(Resultado.Erro(e))
+            }
+        }
+
+    fun edita(post: Post) =
+        liveData<Resultado<Unit>>(viewModelScope.coroutineContext) {
+            try {
+                repository.edita(post)
+                emit(Resultado.Sucesso())
+            } catch (e: Exception) {
+                emit(Resultado.Erro(e))
+            }
+        }
+
+}
